efea88f78d5e90ea0ef4d8952d5e4509362d889f
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-07-31 20:33:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-resource "spacelift_stack" "ec2-stack" {
-
-  autodeploy   = false
-  branch       = "main"
-  description  = "AWS EC2 Instance and Networking"
-  name         = "development-stack"
-  repository   = "tftest"
-  terraform_version = "1.5.0"
-}
-
 resource "spacelift_policy" "admin_login" {
   name     = "Admin-Login-Policy2"
   body     = file("test.rego")
