72c3b75e79410e1c5cc9631d6ba767a389e91bbe
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-07-31 17:29:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 resource "spacelift_stack" "ec2-stack" {
 
-  github_enterprise {
-    namespace = "Daniellem97"
-  }
-
   autodeploy   = false
   branch       = "main"
   description  = "AWS EC2 Instance and Networking"
