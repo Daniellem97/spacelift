6bc6e03acd86232969100706ba48443566925204
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-07-31 20:24:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ resource "spacelift_stack" "ec2-stack" {
 resource "spacelift_policy" "admin_login" {
   name     = "Admin-Login-Policy"
   body     = file("test.rego")
-  space_id = "legacy"
+  space_id = "root"
   type     = "LOGIN"
 }
