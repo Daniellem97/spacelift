0a0db771c85c1cf119303429d5dd8c37a28f49de
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-08-01 09:14:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "spacelift_policy" "admin_login" {
   name     = "Admin-Login-Policy2"
   body     = file("test.rego")
-  space_id = "test"
+  space_id = "test-01H6R294WFHS13BVF89C7W7BG6"
   type     = "LOGIN"
 }
