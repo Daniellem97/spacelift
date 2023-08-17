2c900d5553d5671f2dcab8ecd0183c8a3347f48d
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-08-17 14:00:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "spacelift_policy" "admin_login" {
-  name     = "Admin-Login-Policy2"
-  body     = file("test.rego")
-  space_id = "root"
-  type     = "LOGIN"
-}
-
 resource "spacelift_stack" "ec2-stack" {
 
   github_enterprise {
@@ -15,5 +8,5 @@ resource "spacelift_stack" "ec2-stack" {
   branch       = "main"
   description  = "AWS EC2 Instance and Networking"
   name         = "development-stack"
-  repository   = "aws-ec2"
+  repository   = "tftest"
 }
