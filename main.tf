49dccf0d80abd6f2af626e5e4a5d535e39c1ad11
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-08-31 11:02:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,5 @@ resource "spacelift_stack" "ec2-stack" {
   description  = "AWS EC2 Instance and Networking"
   name         = "development-stack"
   repository   = "tftest"
-  space_id        = "legacyi"
-}
-
-output "spacelift_ec2_stack_space_id" {
-  value = spacelift_stack.ec2-stack.space_id
-  description = "The space_id of the spacelift_stack ec2-stack"
+  space_id        = "legacy"
 }
