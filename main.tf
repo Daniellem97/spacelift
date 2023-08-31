65f25508e52c061d0f76ef2db8e9cce353351c6b
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-08-31 10:45:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,6 @@ resource "spacelift_stack" "ec2-stack" {
   description  = "AWS EC2 Instance and Networking"
   name         = "development-stack"
   repository   = "tftest"
+  space        = "legacy"
 }
 
-data "spacelift_gcp_service_account" "privateaws" {
-  stack_id = "privateaws"
-}
