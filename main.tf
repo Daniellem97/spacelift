394c3b3e1b47098258a163db1fc0d07894b6691d
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-10-06 11:28:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,5 +14,5 @@ resource "spacelift_module" "k8s-module" {
   administrative     = true
   branch             = "main"
   description        = "Infra terraform module"
-  repository         = "terraform-super-module"
+  repository         = "terraformmultimodule"
 }
