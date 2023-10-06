a2f9ba3e14fce17393f6b36538e57cf0538a2f3b
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-10-06 11:25:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "spacelift_module" "k8s-module" {
   name               = "k8s-module"
   terraform_provider = "aws"
   administrative     = true
+  bracnh             = main
   description        = "Infra terraform module"
   repository         = "terraform-super-module"
 }
