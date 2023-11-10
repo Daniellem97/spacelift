6f12f0b86309b35dd5f7f21574132cde7cd0d70b
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-11-10 16:16:04 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "spacelift_stack" "ec2-stack" {
   branch       = "main"
   description  = "AWS EC2 Instance and Networking"
   name         = "development-stack"
-  repository   = "tftest"
+  repository   = "tftest2"
   space_id        = "legacy"
   terraform_version = "1.5.7"
 }
