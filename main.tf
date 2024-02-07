914a637cb9a4a3fcb91bcc60cb51c7bda59ab2b3
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-02-07 17:40:34 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,5 +8,5 @@ resource "spacelift_idp_group_mapping" "test" {
 }
 
 data "spacelift_aws_integration" "testpublic11" {
-  integration_id = "1101H1W12328PM15MNK00G9FHAVV"
+  integration_id = "01H1W12328PM15MNK00G9FHAVV"
 }
