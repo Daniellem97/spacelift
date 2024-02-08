90ffffe8eb4c1eea4b8588d2001201c981b76528
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-02-08 17:46:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,20 @@ resource "spacelift_stack_dependency" "test" {
   stack_id            = spacelift_stack.app.id
   depends_on_stack_id = spacelift_stack.infra.id
 }
+
+resource "spacelift_stack" "frontdoor_dev_02_routing" {
+  # GitHub custom app
+  github_enterprise {
+    namespace = "Daniellem97"
+  }
+
+  name       = "frontdoor/dev/02_routing"
+  autodeploy = true
+  space_id   = root
+
+  # Source code
+  repository                   = "tftest"
+  project_root                 = "dev/02_routing"
+  branch                       = "main"
+  description                  = "Stack for Frontdoor's dev/02_routing resources"
+
