4d84bd26f7dcdbbead5f12a29cd1a8f6e04ad590
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-02-26 09:09:26 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,3 @@
-resource "spacelift_idp_group_mapping" "test" {
-
-  name = "testingsl1"
-  policy {
-    space_id = "root"
-    role     = "READ"
-  }
-}
-
-
-resource "spacelift_stack" "frontdoor_dev_02_routing" {
-  # GitHub custom app
-  github_enterprise {
-    namespace = "Daniellem97"
-  }
-
-  name       = "frontdoor/dev/02_routing"
-  autodeploy = true
-  space_id   = "root"
-
-  # Source code
-  repository                   = "tftest"
-  project_root                 = "dev/02_routing"
-  branch                       = "main"
-  description                  = "Stack for Frontdoor's dev/02_routing resources"
-
-}
 resource "spacelift_space" "development" {
   name = "development"
 
