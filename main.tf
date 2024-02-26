b55daeba45958c0700cabddd8c7e3451dc09fa33
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-02-26 09:06:48 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,3 +25,13 @@ resource "spacelift_stack" "frontdoor_dev_02_routing" {
   description                  = "Stack for Frontdoor's dev/02_routing resources"
 
 }
+resource "spacelift_space" "development" {
+  name = "development"
+
+  # Every account has a root space that serves as the root for the space tree.
+  # Except for the root space, all the other spaces must define their parents.
+  parent_space_id = "root"
+
+  # An optional description of a space.
+  description = "This a child of the root space. It contains all the resources common to the development infrastructure."
+}
