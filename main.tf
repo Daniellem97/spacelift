92a8378cc69fcd026091c069b5a0ceac4bf46e42
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-04 11:57:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "spacelift_stack" "metabase_test" {
   branch            = "main"
   project_root      = "path/to/metabase"
   terraform_version = "1.5.7"
-  space_id = "metabasetest"
+  space_id = "metabasetest2"
   # GitHub Enterprise settings for the stack
   github_enterprise {
     namespace = "Daniellem97" # The GitHub organization or user the repository belongs to
