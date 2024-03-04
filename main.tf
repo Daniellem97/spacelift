4229f9449bce095fd4b57eab3974918d8c726817
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-04 13:24:21 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,9 +60,7 @@ resource "spacelift_stack" "testing3" {
   terraform_version = "1.5.7"
   space_id = "metabasetest-01HBRT2GZTJEVVA4APJDNTNSV1"
   # GitHub Enterprise settings for the stack
-  bitbucket_cloud {
      bitbucket_cloud {
     namespace = "testspacespecific" # The Bitbucket Cloud workspace or team containing the repository
   }
-}
 }
