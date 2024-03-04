2fb312f3e6ad58df5522ca1d2de71ded0ade2876
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-04 13:03:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,26 @@ resource "spacelift_stack" "testing" {
   repository        = "test"
   branch            = "main"
   terraform_version = "1.5.7"
-  space_id = "legacy"
+  space_id = "metabasetest-01HBRT2GZTJEVVA4APJDNTNSV1"
+  # GitHub Enterprise settings for the stack
+  gitlab {
+    namespace = "Daniellem97"
+    id = data.spacelift_gitlab_integration.this.id
+  }
+}
+
+
+data "spacelift_bitbucket_cloud_integration" "this" {
+  id = "Bitbucketspacespecific" 
+}
+
+# Define the stack in Spacelift
+resource "spacelift_stack" "testing" {
+  name              = "blahblahblah"
+  repository        = "testspacespecific"
+  branch            = "main"
+  terraform_version = "1.5.7"
+  space_id = "metabasetest-01HBRT2GZTJEVVA4APJDNTNSV1"
   # GitHub Enterprise settings for the stack
   gitlab {
     namespace = "Daniellem97"
