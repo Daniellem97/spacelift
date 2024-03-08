3080c4b043f915063a7cf774b12924c15e282c73
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-08 11:37:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,15 @@
+variable "labels" {
+  description = "Map of labels to be converted into a set of strings"
+  type        = map(string)
+  default     = {
+    namespace = "foo-bar"
+  }
+}
 
+resource "spacelift_module" "this" {
+  name       = "terraform-default-multimodule2"
+  branch     = "main"
+  repository = "testmultimodule"
+  labels     = toset([for k, v in var.labels : "${k}=${v}"])
+}
 
