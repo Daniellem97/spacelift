3b4ae4de7921c3c9ee5b5f84c2a016a5b6b43aef
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-11 16:28:13 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
-variable "labels" {
-  description = "Map of labels"
-  type        = map(string)
-  default     = {
-    namespace = "foo-bar"
-  }
-}
+resource "spacelift_stack" "this" {
+  branch                  = main
+  name                    = that
+  repository              = tftest
+  space_id                = root
+  terraform_version       = "1.6.2"
+  terraform_workflow_tool = "OPEN_TOFU"
 
-resource "spacelift_module" "this" {
-  name       = "terraform-default-multimodule2"
-  branch     = "main"
-  repository = "testmultimodule"
-  labels     = toset(values(var.labels))
+  labels = [
+    "feature:add_plan_pr_comment"
+  ]
 }
