429be15730d70361010f77f8d272e12ba5c1411f
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-13 16:19:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "spacelift_stack" "this" {
   branch                  = "main"
   name                    = "that"
   repository              = "tftest"
+  space_id                = "root"
   terraform_version       = "1.6.1"
   terraform_workflow_tool = "OPEN_TOFU"
 
