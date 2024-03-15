17a54b1d71d3cec004e41d4d6ef812a213b4fa5f
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-15 21:01:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "spacelift_stack" "this" {
   branch                  = "main"
   name                    = "that"
+  administrative          = "true"
   repository              = "tftest"
   terraform_version       = "1.6.1"
   terraform_workflow_tool = "OPEN_TOFU"
@@ -15,8 +16,4 @@ resource "spacelift_context" "prod-k8s-ie" {
   name        = "Production cluster (Ireland)"
 }
 
-data "spacelift_context_attachment" "production-cluster-ireland" {
-  context_id = "production-cluster-ireland"
-  stack_id   = "apps-cluster"
-}
 
