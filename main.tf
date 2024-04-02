c66866a32371e95687bdb3f4376143b769fa709e
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-04-02 15:04:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 resource "spacelift_environment_variable" "test" {
-  context_id = "prod-k8s-ie"
+  stack_id = "danielle-test"
   name       = "KUBECONFIG"
   value      = "test"
   write_only = false
