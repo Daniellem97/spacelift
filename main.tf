2f83116c3a2542a2e98a82cdbdb3a80a0c9b9136
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-04-02 13:55:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,49 +1,7 @@
-resource "spacelift_stack" "this" {
-  branch                  = "main"
-  name                    = "that"
-  repository              = "tftest"
-  terraform_version       = "1.6.1"
-  terraform_workflow_tool = "OPEN_TOFU"
 
-  labels = [
-    "feature:add_plan_pr_comment"
-  ]
-}
-
-resource "spacelift_stack" "versiontest123" {
-  branch                  = "main"
-  name                    = "versiontest"
-  repository              = "tftest"
-
-  labels = [
-    "feature:add_plan_pr_comment"
-  ]
-}
-
-
-resource "spacelift_context" "prod-k8s-ie" {
-  description = "Configuration details for the compute cluster in 🇮🇪"
-  name        = "Production cluster (Ireland)"
-  space_id    = "root"
-}
-
-
-resource "spacelift_context_attachment" "attachment" {
-  context_id = "production-cluster-ireland"
-  stack_id   = "that"
-}
-
-data "spacelift_worker_pool" "private" {
-  worker_pool_id = "01HNK02PZAPP4YFH4C0JE2J8Y4"
-}
-
-resource "spacelift_worker_pool" "private" {
-  name        = "worker-pool"
-}
-
-resource "spacelift_environment_variable" "ireland-kubeconfig" {
+resource "spacelift_environment_variable" "test" {
   context_id = "prod-k8s-ie"
   name       = "KUBECONFIG"
-  value      = "/project/spacelift/kubeconfig"
+  value      = "test"
   write_only = false
 }
