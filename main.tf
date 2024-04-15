df9fb6ce7a0980e85895906cc71ae4cba82d9c9a
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-04-15 14:08:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,17 +11,39 @@ resource "spacelift_stack" "app1" {
   repository = "tftest"
 }
 
-locals {
-  STACK-DEP = {
-    "infra_depends_on_app1" = {
-      Src = spacelift_stack.infra.id
-      DST = spacelift_stack.app1.id
-    }
+variable "SP_CT" {
+  default = {
+    SPACE_FAMILY   = "myproject"
+    SPACE_NAME     = "production"
+    SPACE_VERSION  = "1-0"
   }
+}
+
+variable "DEPENDENCIES" {
+  default = [
+    {
+      Src = {
+        StackName = "backend"
+      }
+      Dst = {
+        StackName = "frontend"
+      }
+    }
+  ]
+}
+
+locals {
+  STACK-DEP = distinct([for v in var.DEPENDENCIES : {
+    "SrcShort" = v["Src"]["StackName"],
+    "DstShort" = v["Dst"]["StackName"],
+    "Src"      = lower(join("_", [var.SP_CT.SPACE_FAMILY, var.SP_CT.SPACE_NAME, replace(var.SP_CT.SPACE_VERSION, ".", "-"), v["Src"]["StackName"]])),
+    "Dst"      = lower(join("_", [var.SP_CT.SPACE_FAMILY, var.SP_CT.SPACE_NAME, replace(var.SP_CT.SPACE_VERSION, ".", "-"), v["Dst"]["StackName"]])),
+  }])
 }
 
 resource "spacelift_stack_dependency" "stack-dep" {
   for_each            = { for k, v in local.STACK-DEP : k => v }
   depends_on_stack_id = each.value["Src"]
-  stack_id            = each.value["DST"]
+  stack_id            = each.value["Dst"]
 }
+
