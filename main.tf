9eadb88c2d1b25c9f313908a669935db30d02dd9
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-04-15 10:24:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,20 @@ resource "spacelift_module" "that" {
   branch                = "main"
   space_id              = "root"
 }
+
+resource "spacelift_stack" "k8s-core-kubernetes" {
+  kubernetes {
+    namespace       = "core"
+    kubectl_version = "1.26.1" # Optional kubectl version
+  }
+
+  autodeploy   = true
+  branch       = "main"
+  description  = "Shared cluster services (Datadog, Istio etc.)"
+  name         = "Kubernetes core services"
+  project_root = "core-services"
+  repository   = "tftest"
+
+  # You can use hooks to authenticate with your cluster
+  before_init = ["aws eks update-kubeconfig --region us-east-2 --name k8s-cluster"]
+}
