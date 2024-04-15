c3c3ead6baecb7954b6526cbcba280cde1726535
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-04-15 13:36:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,27 +16,3 @@ resource "spacelift_stack_dependency" "test" {
   depends_on_stack_id = spacelift_stack.infra.id
 }
 
-resource "spacelift_module" "that" {
-  repository            = "terraform-aws-module"
-  branch                = "main"
-  space_id              = "root"
-}
-
-resource "spacelift_stack" "k8s-core-kubernetes" {
-  kubernetes {
-    namespace       = "core"
-    kubectl_version = "1.26.1" # Optional kubectl version
-    terraform_workflow_tool  = "CUSTOM"
-  }
-
-  autodeploy   = true
-  branch       = "main"
-  description  = "Shared cluster services (Datadog, Istio etc.)"
-  name         = "Kubernetes core services"
-  project_root = "core-services"
-  repository   = "tftest"
- 
-
-  # You can use hooks to authenticate with your cluster
-  before_init = ["aws eks update-kubeconfig --region us-east-2 --name k8s-cluster"]
-}
