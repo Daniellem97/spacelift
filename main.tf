43fe2abb9f5ec1792c1b8c8e8d5c6eb6797f7808
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-04-19 11:20:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,13 +19,38 @@ resource "spacelift_stack" "frontend" {
 
 }
 
-resource "spacelift_stack" "frontend2" {
-  name       = "myproject_production_1-0_frontend2"
-  branch            = "main"
-  space_id          = "root"
-  description       = "change"
-  project_root      = "cluster"
-  repository        = "tftest"
-  terraform_version = "1.3.0"
+variable "SP_CT" {
+  default = {
+    SPACE_FAMILY   = "myproject"
+    SPACE_NAME     = "production"
+    SPACE_VERSION  = "1-0"
+  }
+}
 
+variable "DEPENDENCIES" {
+  default = [
+    {
+      Src = {
+        StackName = "backend"
+      }
+      Dst = {
+        StackName = "frontend"
+      }
+    }
+  ]
+}
+
+locals {
+  STACK-DEP = distinct([for v in var.DEPENDENCIES : {
+    "SrcShort" = v["Src"]["StackName"],
+    "DstShort" = v["Dst"]["StackName"],
+    "Src"      = lower(join("_", [var.SP_CT.SPACE_FAMILY, var.SP_CT.SPACE_NAME, replace(var.SP_CT.SPACE_VERSION, ".", "-"), v["Src"]["StackName"]])),
+    "Dst"      = lower(join("_", [var.SP_CT.SPACE_FAMILY, var.SP_CT.SPACE_NAME, replace(var.SP_CT.SPACE_VERSION, ".", "-"), v["Dst"]["StackName"]])),
+  }])
+}
+
+resource "spacelift_stack_dependency" "stack-dep" {
+  for_each            = { for k, v in local.STACK-DEP : k => v }
+  depends_on_stack_id = each.value["Src"]
+  stack_id            = each.value["Dst"]
 }
