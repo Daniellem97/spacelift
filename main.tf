b344e457d4b85ce6df2c348496ba023612996c06
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-06-14 12:40:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+# Spacelift Stack Resource
+resource "spacelift_stack" "example_stack" {
+  name        = "example-stack1"
+  repository  = "tftest"
+  branch      = "main"
+  description = "An example Spacelift stack"
+}
 
+# AWS S3 Bucket Resource
+resource "aws_s3_bucket" "example_bucket1232423" {
+  bucket = "example-bucket-unique-name1231231"  # Ensure this name is globally unique
+  acl    = "private"
+
+  tags = {
+    Name        = "example-bucket"
+    Environment = "Dev"
+  }
+}
