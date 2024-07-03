919e091a09415ea45ad7eda2beebb000a12660ea
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-07-03 10:09:46 +0100

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+resource "random_string" "example" {
+  length  = 16
+  special = false
+}
+
+output "random_string_output" {
+  description = "A random string"
+  value       = random_string.example.result
+}
