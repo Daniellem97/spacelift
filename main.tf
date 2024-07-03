731764b8329d4e9cdbe857c17548e1a0f9ba760d
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-07-03 10:14:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 # Spacelift Stack Resource
 resource "spacelift_stack" "example_stack" {
+ github_enterprise {
+    namespace = "spacelift" # The GitHub organization / user the repository belongs to
+  }
   name        = "example-stack1"
   repository  = "tftest"
   branch      = "main"
