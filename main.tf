d2586713355cca91171c7a11a1bfd826a06c5c60
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-08-27 10:18:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,23 +9,16 @@ resource "spacelift_stack" "example_stack" {
   description = "An example Spacelift stack"
 }
 
+resource "spacelift_environment_variable" "core-kubeconfig" {
+  stack_id    = "example_stack"
+  name        = "KUBECONFIG"
+  value       = "/project/spacelift/kubeconfig"
+  write_only  = false
+  description = "Kubeconfig for the core stack"
+}
+
 resource "spacelift_context" "prod-k8s-ie" {
   description = "Configuration details for the compute cluster in 🇮🇪"
   name        = "Production cluster (Ireland)"
-}
-
-resource "spacelift_stack" "this" {
- github_enterprise {
-    namespace = "Daniellem97"
-  }
-
-branch = "main"
-name = "labelsdebug"
-repository = "securitygroups"
-labels            = local.labels
-}
-
-locals {
-  labels              = ["stack:terraform"]
 }
 
