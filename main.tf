2261307368b930529be39874afff28ada99e60f1
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-09-17 13:06:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,29 +9,9 @@ resource "spacelift_stack" "example_stack" {
   description = "An example Spacelift stack"
 }
 
-resource "spacelift_policy" "test_modification" {
-  name = data.terraform_remote_state.debugtest.outputs.projects.test1.id
-  body = file("./policies/login.rego")
-  type = "LOGIN"
-}
-
-data "terraform_remote_state" "debugtest" {
-  backend = "remote"
-
-  config = {
-    hostname     = "spacelift.io"
-    organization = "daniellem97"
-
-    workspaces = {
-      name = "debugtest"
-    }
-  }
-}
-
-output "test1_id" {
-  value = data.terraform_remote_state.debugtest.outputs.projects.test1.id
-}
-
-output "test2_number" {
-  value = data.terraform_remote_state.debugtest.outputs.projects.test2.number
+resource "spacelift_terraform_provider" "provider" {
+  # This is the type of your provider.
+  type        = "myinternaltool"
+  space_id    = "root"
+  description = "Explain what this is for"
 }
