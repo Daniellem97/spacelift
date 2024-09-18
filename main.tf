cbe111efa95c4553756dd4e425ee08522833ee54
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-09-18 18:12:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,3 @@ resource "spacelift_stack" "example_stack" {
   branch      = "main"
   description = "An example Spacelift stack"
 }
-
-resource "spacelift_terraform_provider" "provider" {
-  # This is the type of your provider.
-  type        = "myinternaltool"
-  space_id    = "root"
-  description = "Explain what this is for"
-}
