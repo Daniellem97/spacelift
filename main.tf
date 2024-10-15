dbe2a5470119e7cb8d1b5b8a9bfbd0ba2f03bbd1
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-10-15 15:45:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "spacelift_stack" "example_stack" {
   repository  = "securitygroups"
   branch      = "main"
   space_id    = "testnoinheritance-01J82YD2J1E8BRQAR8E1WS3075"
+  github_action_deploy = false
   description = "An example Spacelift stack"
 }
 
