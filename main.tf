59d8595f647dcddc9aee2168a96915f2d1366405
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-01-14 09:48:16 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,18 +11,6 @@ resource "spacelift_stack" "example_stack" {
   description = "An example Spacelift stack"
 }
 
-resource "spacelift_stack" "example_stack" {
- github {
-    id = "github-enterprise-default-integration"
-    namespace = "Daniellem97"
-  }
-  name        = "example-stack"
-  repository  = "securitygroups"
-  branch      = "main"
-  space_id    = "testnoinheritance-01J82YD2J1E8BRQAR8E1WS3075"
-  github_action_deploy = false
-  description = "An example Spacelift stack"
-}
 
 resource "spacelift_stack" "ansible_tableau_update_os" {
   ansible {
