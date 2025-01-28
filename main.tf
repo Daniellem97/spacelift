565aea69498b6d95ddff0bffe91181522a3fc141
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-01-28 12:59:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
-resource "spacelift_stack" "example_stack2" {
+# Spacelift Stack
+resource "spacelift_stack" "example_stack" {
+  count = var.enabled ? 1 : 0
+
   github_enterprise {
     namespace = "Daniellem97"
   }
@@ -6,10 +9,27 @@ resource "spacelift_stack" "example_stack2" {
   name                     = "example-stack"
   repository               = "securitygroups"
   branch                   = "main"
-  github_action_deploy     = false
   description              = "An example Spacelift stack"
+
+  # Dynamically depend on the destructor during deletion
+  depends_on = var.enabled ? [] : [null_resource.ensure_destructor_run]
 }
 
+# Spacelift Destructor
 resource "spacelift_stack_destructor" "example_stack_destructor" {
-  stack_id      = spacelift_stack.example_stack2.id
+  count        = var.enabled ? 0 : 1
+  stack_id     = spacelift_stack.example_stack[0].id
+}
+
+# Dummy Resource to Enforce Destructor Dependency
+resource "null_resource" "ensure_destructor_run" {
+  count = var.enabled ? 0 : 1
+
+  depends_on = [
+    spacelift_stack_destructor.example_stack_destructor
+  ]
+
+  provisioner "local-exec" {
+    command = "echo 'Destructor has run and cleaned up resources.'"
+  }
 }
