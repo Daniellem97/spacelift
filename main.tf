6736e09d62678da6be20457cd26818d31f81eb66
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-01-28 15:42:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,1 @@
-resource "spacelift_stack" "example_stack" {
-  github_enterprise {
-    namespace = "Daniellem97"
-  }
-  name                     = "example-stack"
-  repository               = "securitygroups"
-  branch                   = "main"
-  description              = "An example Spacelift stack"
-}
 
-# Spacelift Destructor
-resource "spacelift_stack_destructor" "example_stack_destructor" {
-  stack_id     = spacelift_stack.example_stack.id
-}
