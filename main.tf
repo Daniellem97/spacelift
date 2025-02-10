495f50a322780fd7deb8b077c1157aea77e50d8b
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-02-10 11:38:26 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ resource "spacelift_audit_trail_webhook" "example" {
   endpoint = "https://webhook.site/bc8524c1-a1fb-4204-b9b2-2eba23642508"
   enabled  = true
   secret   = "mysecretkey"
+lifecycle {
+    ignore_changes = [value]
+  }
 }
