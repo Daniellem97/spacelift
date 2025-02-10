035d79f7857593f6079a5ac5b655fb3b4ff4df61
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-02-10 11:18:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,5 @@
-resource "spacelift_stack" "testterragrunt" {
-  terragrunt {
-    terraform_version      = "1.7.2"
-    terragrunt_version     = "0.59.6"
-    use_run_all            = false
-    use_smart_sanitization = true
-    tool                   = "OPEN_TOFU"
-  }
-
-  space_id = "production-01JAWQ7K45F8TDK53CKN6GDV85"
-  administrative = true
-  branch       = "main"
-  name         = "testingname"
-  repository   = "terragrunttest"
+resource "spacelift_audit_trail_webhook" "example" {
+  endpoint = "https://example.com"
+  enabled  = true
+  secret   = "mysecretkey"
 }
