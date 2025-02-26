2073fec822799e3ccb1740c103b5d31fd7bce8b1
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-02-26 16:21:49 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,1 @@
 
-# Spacelift Stack Definition
-resource "spacelift_stack" "example_stack" {
-  name             = "example-stack-1-worker"
-  repository       = "tftest"
-  branch           = "main"
-  terraform_version = "1.5.0"
-  # Auto-deploy on changes
-  autodeploy = true
-  # Description
-  description = "An example Spacelift stack with a destructor attached."
-}
