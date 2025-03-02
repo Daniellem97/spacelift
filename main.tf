e25a9f0c4ba68da528e9e3862dbbef73cec826dc
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-03-02 14:56:25 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
-
+resource "spacelift_stack" "example" {
+  name         = "environmentvariablecontext"
+  description  = "A stack to show the environment variable priority"
+  branch       = "main"
+  raw_git {
+    url        = "https://github.com/spacelift-io/terraform-provider-spacelift"
+    branch     = "main"
+  }
+}
