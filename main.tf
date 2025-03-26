ab0b3a26cb66577e8419d13975957338cc6e6a52
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-03-26 17:37:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,4 @@
 resource "spacelift_context" "example" {
   name = "example-context"
   description = "Runs custom script after plan step"
-
-  after_plan = [
-    "echo 'Plan just completed!'",
-    "cat .terraform/plan.out > /tmp/plan-output.txt",
-    "your-custom-script.sh"
-  ]
 }
