b86bae2b567aa07bcbc4f3707e33cef7d64ae5b9
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-03-27 20:03:33 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
-
-resource "spacelift_context" "example" {
-  name = "example-context"
-  description = "Runs custom script after plan step"
+locals {
+  modules = csvdecode(file("${path.module}/terraform_modules.csv"))
 }
 
+resource "spacelift_module" "from_csv" {
+  for_each = {
+    for module in local.modules : module.module_name => module
+    if module.spacelift_module_required == "true"
+  }
+
+  name        = each.value.module_name
+  description = "Terraform module for ${each.value.project}"
+
+  # Optional fields depending on your setup
+  repository = each.value.repository_creation_required == "true" ? each.value.module_name : null
+
+  # Add other optional fields if necessary
+  labels = [each.value.project]
+}
