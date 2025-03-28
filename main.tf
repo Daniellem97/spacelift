8e3e71bd7f3cc18fb1897aff348ffdb47fee5dae
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-03-28 18:18:34 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
-locals {
-  modules = csvdecode(file("${path.module}/terraform_modules.csv"))
-}
-
-resource "spacelift_module" "from_csv" {
-  for_each = {
-    for module in local.modules : module.module_name => module
-    if module.spacelift_module_required == "true"
-  }
-
-  name        = each.value.module_name
-  description = "Terraform module for ${each.value.project}"
-  branch      = "main"
-  repository  = "testmultimodule"
-  labels      = [each.value.project]
+resource "spacelift_space" "development" {
+  name = "development"
+  parent_space_id = "root"
 }
