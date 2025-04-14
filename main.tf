b0d33b36d7df02462c51b7eef7f1e66a66eb94ee
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-04-14 15:21:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
-resource "spacelift_space" "development" {
-  name = "development3"
-  parent_space_id = "root"
+resource "spacelift_mounted_file" "aws_region_vars" {
+  name     = "terraform.tfvars"
+  content  = <<EOT
+aws_region = "us_west_1"
+EOT
+  write_only = false
+}
+
+resource "spacelift_context" "prod-k8s-ie" {
+  description = "Configuration details for the compute cluster in 🇮🇪"
+  name        = "Production cluster (Ireland)"
+  label       = "autoattach:aws"
+
+  mounted_file_ids = [
+    spacelift_mounted_file.aws_region_vars.id
+  ]
+
+  before_init {
+    command = <<EOT
+      mkdir -p "/mnt/workspace/source/${TF_VAR_project_root}"
+      mv /mnt/workspace/terraform.tfvars "/mnt/workspace/source/${TF_VAR_project_root}/terraform.tfvars"
+    EOT
+  }
 }
