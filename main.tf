69bf7a2e560b8eb38ec6b54cc7fd2051383cffd1
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-04-14 15:38:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,4 +11,8 @@ resource "spacelift_context" "prod-k8s-ie" {
   description = "Configuration details for the compute cluster in 🇮🇪"
   name        = "Production cluster (Ireland)"
   labels       = ["autoattach:aws"]
+before_init = [
+    "mkdir -p \"/mnt/workspace/source/${TF_VAR_project_root}\"",
+    "mv /mnt/workspace/terraform.tfvars \"/mnt/workspace/source/${TF_VAR_project_root}/terraform.tfvars\""
+  ]
 }
