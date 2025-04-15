ecca0b3b11df5dfd4f7670756a923b849161d661
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-04-15 10:18:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "spacelift_mounted_file" "ireland-kubeconfig" {
   context_id    = spacelift_context.prod_k8s_ie.id
+  space_id = "root"
   relative_path = "source/terraform.auto.tfvars"
   content = base64encode("environment = \"whatever\"")
   write_only = false
@@ -9,6 +10,7 @@ resource "spacelift_mounted_file" "ireland-kubeconfig" {
 resource "spacelift_context" "prod_k8s_ie" {
   description = "Configuration details for the compute cluster in 🇮🇪"
   name        = "Production cluster (Ireland)"
+  space_id = "root"
   labels       = ["autoattach:aws"]
 before_init = [
     "mkdir -p \"/mnt/workspace/source/$TF_VAR_spacelift_project_root\"",
