a59bdd7194cd1340297945cc521c685a0a681422
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-05-19 19:21:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,18 @@ resource "spacelift_mounted_file" "ireland-kubeconfig" {
   write_only = false
 }
 
+resource "spacelift_policy" "trigger_only_stacks_project_root" {
+  name     = "trigger only stacks project root"
+  type     = "trigger" 
+  body     = file("test.rego")
+  space_id = "root"  # or whatever space ID you use
+}
+
+import {
+  to = spacelift_policy.trigger_only_stacks_project_root
+  id = "trigger-only-stacks-project-root"
+}
+
 resource "spacelift_module" "k8s-module" {
   name               = "k8s-module"
   terraform_provider = "aws"
