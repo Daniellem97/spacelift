95aa1cb65764d74ca4d66030d4c2f4a8f662296c
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-06-17 12:34:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-resource "spacelift_mounted_file" "ireland-kubeconfig" {
-  context_id    = spacelift_context.prod_k8s_ie.id
-  relative_path = "source/terraform.auto.tfvars"
-  content = base64encode("environment = \"whatever\"")
-  write_only = false
-}
-
-resource "spacelift_stack" "testtg" {
-  name = "testtg"
-  space_id = "root"
-
-  namespace = "Daniellem97"
-  repository = "AddressBook"
-  branch = "master"
-
-  terragrunt {
-    terraform_version = "1.5.7"
-    terragrunt_version = "0.73.8"
-    tool = "TERRAFORM_FOSS"
-    use_run_all = false
-    use_smart_sanitization = true
-  }
-
-  enable_well_known_secret_masking = true
-  github_action_deploy = false
-}
 
 resource "spacelift_policy" "trigger_only_stacks_project_root" {
   name     = "trigger only stacks project root"
