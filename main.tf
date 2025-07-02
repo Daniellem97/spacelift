6501de5733a6559ba8a6a875fb7b6460b6ae4b3f
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-07-02 11:10:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,10 +35,32 @@ resource "spacelift_idp_group_mapping" "test" {
 }
 
 locals {
-  spaces = {
-    for i in range(3600) :
+  spaces_chunk_1 = {
+    for i in range(0, 1024) :
     format("space-%04d", i) => "This is space number ${i}"
   }
+
+  spaces_chunk_2 = {
+    for i in range(1024, 2048) :
+    format("space-%04d", i) => "This is space number ${i}"
+  }
+
+  spaces_chunk_3 = {
+    for i in range(2048, 3072) :
+    format("space-%04d", i) => "This is space number ${i}"
+  }
+
+  spaces_chunk_4 = {
+    for i in range(3072, 3600) :
+    format("space-%04d", i) => "This is space number ${i}"
+  }
+
+  spaces = merge(
+    local.spaces_chunk_1,
+    local.spaces_chunk_2,
+    local.spaces_chunk_3,
+    local.spaces_chunk_4,
+  )
 }
 
 resource "spacelift_space" "many_spaces" {
