4a5532a27f36affe1344db53f1fece40ca5d1b6b
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-07-02 11:19:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-
-resource "spacelift_policy" "trigger_only_stacks_project_root" {
-  name     = "trigger only stacks project root"
-  type     = "GIT_PUSH" 
-  body     = file("test.rego")
-  space_id = "root"  # or whatever space ID you use
-}
-
-import {
-  to = spacelift_policy.trigger_only_stacks_project_root
-  id = "trigger-only-stacks-project-root"
-}
-
 resource "spacelift_module" "k8s-module" {
   name               = "k8s-module"
   terraform_provider = "aws"
