8ad0adcdca8159561b0a9f64df031aa5828bc75a
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-07-04 15:46:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,28 @@ resource "spacelift_stack" "k8s_cluster2" {
   terraform_version = "1.3.3"
 }
 
+resource "spacelift_stack" "k8s_cluster3" {
+  administrative    = true
+  autodeploy        = true
+  branch            = "main"
+  description       = "Provisions a Kubernetes cluster"
+  name              = "Kubernetes Cluster"
+  project_root      = "cluster"
+  repository        = "tftest"
+  terraform_version = "1.3.3"
+}
+
+resource "spacelift_stack" "k8s_cluster4" {
+  administrative    = true
+  autodeploy        = true
+  branch            = "main"
+  description       = "Provisions a Kubernetes cluster"
+  name              = "Kubernetes Cluster"
+  project_root      = "cluster"
+  repository        = "tftest"
+  terraform_version = "1.3.3"
+}
+
 resource "spacelift_environment_variable" "credentials" {
   stack_id   = spacelift_stack.k8s_cluster2.id
   name       = "AWS_CREDENTIALS"
