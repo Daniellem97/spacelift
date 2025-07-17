61737daebff618f3abd50cef76b1027af6bcb76a
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-07-17 13:45:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,5 +13,4 @@ labels = ["folder:Infrastructure", "kind:meta", "sensitive"]
 administrative = true
 enable_well_known_secret_masking = true
 github_action_deploy = false
-before_init = ["export SPACELIFT_STACK_LABELS=$TF_VAR_spacelift_stack_labels", "${TF_VAR_spacelift_workspace_root}/source/.ci/setup.sh"]
 }
