8befdbd285d671526126400c6abbbf2911fb4a3a
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-07-17 13:44:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,17 @@
-resource "spacelift_stack" "k8s_cluster9" {
-  administrative    = true
-  autodeploy        = true
-  branch            = "main"
-  description       = "Provisions a Kubernetes cluster"
-  name              = "Kubernetes Cluster 2"
-  project_root      = "cluster"
-  repository        = "tftest"
-  terraform_version = "1.3.3"
-}
+resource "spacelift_stack" "spacelift" {
+name = "spacelift"
+space_id = "root"
+repository = "tftest"
+branch = "main"
 
-resource "spacelift_stack" "k8s_cluster3" {
-  administrative    = true
-  autodeploy        = true
-  branch            = "main"
-  description       = "Provisions a Kubernetes cluster"
-  name              = "Kubernetes Cluster 3 "
-  project_root      = "cluster"
-  repository        = "tftest"
-  terraform_version = "1.3.2"
-}
+terraform_version = "1.5.7"
+terraform_workflow_tool = "CUSTOM"
+terraform_smart_sanitization = true
 
-resource "spacelift_stack" "k8s_cluster4" {
-  administrative    = true
-  autodeploy        = true
-  branch            = "main"
-  description       = "Provisions a Kubernetes cluster"
-  name              = "Kubernetes Cluster 4"
-  project_root      = "cluster"
-  repository        = "tftest"
-  terraform_version = "1.3.3"
+description = "The administrative spacelift stack, which manages our spacelift resources."
+labels = ["folder:Infrastructure", "kind:meta", "sensitive"]
+administrative = true
+enable_well_known_secret_masking = true
+github_action_deploy = false
+before_init = ["export SPACELIFT_STACK_LABELS=$TF_VAR_spacelift_stack_labels", "${TF_VAR_spacelift_workspace_root}/source/.ci/setup.sh"]
 }
